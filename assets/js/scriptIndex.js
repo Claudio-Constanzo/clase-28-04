4c812450ecb41496373b00919e9a89200b9f4743
Claudio-Constanzo <claudio.constanzo02@gmail.com>
2025-04-28 14:27:59 -0400

    validacion

--- assets/js/scriptIndex.js
+++ assets/js/scriptIndex.js
@@ -4,4 +4,11 @@ function validar(){
     console.log(eNombre)
     let vNombre = eNombre.value 
     console.log(vNombre)
+    console.log(vNombre.length)
+    if(vNombre.length > 3){
+        console.log("Cumple")
+    }else{
+        console.log("error pocos caracteres")
+        alert("Debes ingresar al menos mas de 3 caracteres")
+    }
 }
