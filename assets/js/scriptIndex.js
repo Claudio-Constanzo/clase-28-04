f11e348e29c277ab27b5ae7216330256a9048cab
Claudio-Constanzo <claudio.constanzo02@gmail.com>
2025-05-05 14:32:27 -0400

    muchos cambios

--- assets/js/scriptIndex.js
+++ assets/js/scriptIndex.js
@@ -1,11 +1,17 @@
 function validar(){
-    console.log("Hi")
+    // console.log("Hi")
     let eNombre = document.getElementById("nombre")
-    console.log(eNombre)
     let vNombre = eNombre.value.trim()
     let eErrorNombre = document.getElementById("errorNombre")
-    console.log(vNombre)
-    console.log(vNombre.length)
+
+    let eApellido = document.getElementById("apellido")
+    let vApellido = eApellido.value.trim()
+    let eErrorApellido = document.getElementById("errorApellido")
+    // console.log(eNombre)
+    
+    
+    // console.log(vNombre)
+    // console.log(vNombre.length)
     if(vNombre.trim().length==0){
             console.log("Debes completar este campo")
     }
@@ -15,6 +21,17 @@ function validar(){
         eNombre.style.backgroundColor = "green"
         eNombre.style.color = "white"
     }
+    else if(vApellido.length > 3){
+        eErrorApellido.innerText = ""
+        eNombre.style.backgroundColor = "green"
+        eNombre.style.color = "white"
+    }else if(vApellido < 3){
+        console.log("error pocos caracteres")
+        alert("Debes ingresar al menos mas de 3 caracteres")
+        eErrorApellido.innerText = "Debes ingresar 3 caracteres como minimo"
+        eApellido.style.backgroundColor = "red"
+        eApellido.style.color = "white"
+    }
     else{
         console.log("error pocos caracteres")
         alert("Debes ingresar al menos mas de 3 caracteres")
@@ -22,4 +39,5 @@ function validar(){
         eNombre.style.backgroundColor = "red"
         eNombre.style.color = "white"
     }
+   
 }
