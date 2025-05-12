7f045b85afd0bc523ee6d8aed739ae48d94e3086
Claudio-Constanzo <claudio.constanzo02@gmail.com>
2025-05-12 14:08:03 -0400

    Update scriptIndex.js

--- assets/js/scriptIndex.js
+++ assets/js/scriptIndex.js
@@ -9,13 +9,23 @@ function validar(){
     let vApellido = eApellido.value
     let eErrorApellido = document.getElementById("errorApellido")
     // console.log(eNombre)
-    
+    let vlmNombre = validarLargoMinimo(eNombre,vNombre,eErrorNombre)
+    let vlmApellido = validarLargoMinimo(eApellido,vApellido,eErrorApellido)
+    if(vlmNombre == true && vlmApellido == true){
+    //if(vlmNombre && vlmApellido){ cualquiera de las dos sirve , ya que ya retornan true
+        let persona = {
+            nombre : vNombre,
+            apellido : vApellido
+        }
+        personas.push()
+    }
     
     // console.log(vNombre)
     // console.log(vNombre.length)
 
     validarLargoMinimo(eNombre,vNombre,eErrorNombre)
     validarLargoMinimo(eApellido,vApellido,eErrorApellido)
+
 }
 function validarLargoMinimo(elemento,valor,eError){
     if(valor.length > 3){
@@ -23,11 +33,14 @@ function validarLargoMinimo(elemento,valor,eError){
         eError.innerText = ""
         elemento.style.backgroundColor = "green"
         elemento.style.color = "white"
+        return true // true es que funciona(se podria poner lo que sea, 
+        //string, numero,etc, uno interpreta la mejor manera)
     }else{
         console.log("error pocos caracteres")
         alert("Debes ingresar al menos mas de 3 caracteres")
         eError.innerText = "Debes ingresar 3 caracteres como minimo"
         elemento.style.backgroundColor = "red"
         elemento.style.color = "white"
+        return false // false no funciona
 }
 }
